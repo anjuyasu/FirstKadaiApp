d7978ff75a93192a6a2bd9a0215ad901828321d3
anju.yasu <jiblil0verpian0@gmail.com>
2020-06-30 20:51:30 +0900

    Initial Commit

---
+++ FirstKadaiAp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiAp
+//
+//  Created by 安杏樹 on 2020/06/30.
+//  Copyright © 2020 anju.yasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
